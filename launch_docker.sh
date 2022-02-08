20a33ba9843bfd10bc3badca76df1261ce3fdfa3
tribhi <47043059+tribhi@users.noreply.github.com>
2022-02-08 18:21:49 -0500

    Update launch_docker.sh

--- launch_docker.sh
+++ launch_docker.sh
@@ -17,7 +17,7 @@ docker run -it --rm \
     --env="DISPLAY=$DISPLAY" \
     --env="QT_X11_NO_MITSHM=1" \
     --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
-    --volume="${PWD}$/data:/home/catkin_ws/src/habitat_ros_interface/data" \
+    --volume="${PWD}/data:/home/catkin_ws/src/habitat_ros_interface/data" \
     --env="XAUTHORITY=$XAUTH" \
     --volume="$XAUTH:$XAUTH" \
     --gpus all \
